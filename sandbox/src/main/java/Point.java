a93e57be103f4623dc07d58c65b96064cca12347
KamilyaRuslangalieva <138060229+KamilyaRuslangalieva@users.noreply.github.com>
2023-07-13 23:24:25 +0400

    Task2 п.4 - методы

--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -1,4 +1,8 @@
 public class Point {
     public double p1; //координаты точки p1(1;8)
     public double p2; //координаты точки p2(10;17)
+
+    public double distance() {
+        return Math.sqrt(this.p1 + this.p2);
+    }
 }
--- sandbox/src/main/java/Task2.java
+++ sandbox/src/main/java/Task2.java
@@ -1,15 +1,13 @@
 public class Task2 {
     public static void main(String[] args) {
 
-        Point p= new Point();
-        p.p1=Math.pow((10-1),2);
-        p.p2=Math.pow((17-8),2);
+        Point p = new Point();
+        p.p1 = Math.pow((10 - 1), 2);
+        p.p2 = Math.pow((17 - 8), 2);
 
-        System.out.println("Расстояние между двумя точками p1 и p2" + "=" + distance(p));
+        System.out.println("Расстояние между двумя точками p1 и p2" + "=" + p.distance());
     }
-
-
-        public static double distance(Point p) {
-            return Math.sqrt(p.p1+p.p2);
-        }
 }
+
+
+
